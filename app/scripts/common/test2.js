afc560da01e747c5d3f269c6bec9cbee7ddb2976
Mikhail Omelko <omelkom@gmail.com>
2019-10-16 22:19:47 +1000

    further update

--- app/scripts/common/test2.js
+++ app/scripts/common/test2.js
@@ -1,1 +1,35 @@
-console.log('test2');
+"use strict";
+ 
+// console.log('test2');
+
+// let answer = +prompt("How old are you? :");
+
+// console.log(answer);
+// console.log(typeof answer);
+
+// let incr = 10,
+// 	decr = 10;
+
+
+// console.log(incr++);
+// console.log(decr--);
+
+// console.log(incr);
+// console.log(decr);
+
+// let tr = true,
+// 	fl = false;
+
+// 	console.log(tr && fl);
+
+// 	let a = 1,
+// 		b = 2,
+// 		c = 3,
+// 		d = 4;
+
+// console.log(a > b && c > d);
+// console.log(a > b && c < d);
+// console.log(a < b || c > d);
+
+a = 5;
+console.log(a);
